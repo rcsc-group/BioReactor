32967e631c4bf09e34c41360cedc4350e19eec85
Minki Kim <mkkim400@gmail.com>
2025-03-31 22:14:33 -0400

    Codes + Readme

---
+++ Codes/BioReactor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NAME='BioReactor'
+
+for MIN_LEVEL in 7; do
+    for MAX_LEVEL in 9; do
+	
+	rm -rf Data_all Data_specific Fig_vor Fig_vol Fig_tr Fig_oxy
+	mkdir Data_all Data_specific Fig_vor Fig_vol Fig_tr Fig_oxy
+
+	qcc -I -O2 -w -fopenmp -Wall BioReactor.c -o $NAME -L$BASILISK/gl -lglutils -lfb_tiny -lm
+	export OMP_NUM_THREADS=2
+
+	# Bioreactor
+	# 1. Width
+	# 2. Rocking angle (degree)
+	# 3. Rocking rpm
+	# 4. MINLEVEL
+	# 5. MAXLEVEL	
+	
+	./$NAME 0.25 7 37.5
+    done
+done
